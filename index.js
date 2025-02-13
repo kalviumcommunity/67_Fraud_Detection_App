0464c1a4817beb9a54d75c716f4ba0f337d2f761
Moulyaamohankumar <moulyaa2626@gmail.com>
2025-02-13 12:36:17 +0530

    done

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require('express');
-const { connectDB } = require('./Database/db');
+const connectDB  = require('../db');
+const router = require('./routec');
+
 
 const app = express();
-
 require('dotenv').config();
+
 const port = process.env.PORT || 8080;
 const url = process.env.db_url;
 
+app.use(express.json()); 
+app.use('/route', router); 
 
-app.listen(port, async() => {
-
-  try{
+app.listen(port, async () => {
+  try {
     await connectDB(url);
-    console.log(`Server is running on port ${port}`);
-  }
-  catch(error){
+    console.log(`🚀 Server is running on port ${port}`);
+  } catch (error) {
     console.error(error);
   }
-});
-
-
-app.get('/', (req, res) => {    
-    res.send('Hello World!');
 });
